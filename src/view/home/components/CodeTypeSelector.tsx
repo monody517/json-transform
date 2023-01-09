27ddb0805113876bb7ee64ec6d127fd613c8bd27
wenpei <2737716714@qq.com>
2023-01-09 10:33:53 +0800

    添加切换代码风格按钮

--- src/view/home/components/CodeTypeSelector.tsx
+++ src/view/home/components/CodeTypeSelector.tsx
@@ -1,11 +1,18 @@
 import {Popover} from "antd";
+import {CaretDownOutlined} from "@ant-design/icons";
 
 
 const CodeTypeSelector = () => {
 
   const content = (
     <div>
-      <div className={'content-item'}>TypeScript</div>
+      <div
+        style={{
+          cursor: 'pointer',
+        }}
+      >
+        TypeScript
+      </div>
     </div>
   )
 
@@ -20,10 +27,20 @@ const CodeTypeSelector = () => {
       <div style={{
         width: '50%',
         backgroundColor: '#2c3e50'
-      }} />
+      }}/>
       <div>
         <Popover placement="bottom" content={content}>
-          <div>TypeScript</div>
+          <div
+            style={{
+              cursor: 'pointer',
+              fontSize: 16,
+              color: 'white',
+              marginLeft: 12,
+            }}
+          >
+            TypeScript
+            <CaretDownOutlined style={{marginLeft: 4}}/>
+          </div>
         </Popover>
       </div>
     </div>
